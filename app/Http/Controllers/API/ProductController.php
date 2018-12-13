1f08ee2358c7cb79a44a60670574cf6fae2a124a
Edward Simarmata <edwardsaragih97@gmail.com>
2018-12-14 05:25:32 +0700

    product controller

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -17,6 +17,41 @@ class ProductController extends Controller
         return Product::all();
     }
 
+    public function store(Request $request)
+    {
+        $uploadedImages = $request->file('images');
+        $imageNames = [];
+
+        foreach ($uploadedImages as $image) {
+            $imageName = time() . $image->getClientOriginalName();
+
+            array_push($imageNames, $imageName);
+
+            $destinationPath = public_path('/images');
+            $image->move($destinationPath, $imageName);
+        }
+
+        $product = new Product();
+        $product->user_id = Auth::user()->id;
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->sold = 0;
+        $product->category = $request->category;
+        $product->specification = json_encode([
+            'dimention' => $request->dimention,
+            'weight' => $request->weight
+        ]);
+        $product->description = $request->description;
+        $product->color = $request->color;
+        $product->images = json_encode($imageNames);
+        $success=$product->save();
+
+        if(!$success){
+            return Response::json("error solving", 500);
+        }
+            return Respnse::json("success", 201);
+    }
     public function getProducts($id) {
         return response()->json(Product::where('user_id', $id)->get());
     }
