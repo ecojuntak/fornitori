8bb4c2dd5b6d7791054bc98b8e33ad81d20d441e
Edward Simarmata <edwardsaragih97@gmail.com>
2018-12-13 22:34:30 +0700

    api for product

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        return Product::all();
+    }
+
+    public function getProducts($id) {
+        return response()->json(Product::where('user_id', $id)->get());
+    }
+
+    public function searchProduct(Request $request) {
+        $products = Product::with('merchant.profile')->where('name', 'LIKE', '%'. $request->keyword .'%')->get();
+        return response()->json($products);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('products', 'API\ProductController');
+Route::get('merchant/products/{id}', 'API\ProductController@getProducts');
+Route::resource('/transactions', 'API\TransactionController');
+Route::resource('carts', 'API\CartController');
+Route::get('/carts/user/{id}', 'API\CartController@getUserCart');
+Route::get('/provincies', 'API\RegionalController@getProvinces');
+Route::get('/cities', 'API\RegionalController@getCities');
+Route::get('/subdistricts', 'API\RegionalController@getSubdistricts');
+Route::post('/shippingcost', 'API\RajaOngkirController@getShippingCost');
+Route::get('/merchant/{id}/new-orders', 'API\OrderController@getNewOrdersByMerchant');
+Route::get('/merchant/{id}/onprocess-orders', 'API\OrderController@getOnProcessOrdersByMerchant');
+Route::post('/orders/{id}/update-shipping-number', 'API\OrderController@updateShippingNumber');
+Route::post('/merchant/orders/{id}', 'API\OrderController@updateOrderStatus');
+Route::get('/customer/{id}/transactions', 'API\TransactionController@getCustomerTransaction');
+Route::get('/customer/{userId}/transaction/{tranId}', 'API\TransactionController@getTransaction');
+Route::post('/transaction/{id}/proof-of-payment', 'API\TransactionController@updateProofOfPayment');
+Route::post('/transaction/{id}/update-status', 'API\TransactionController@updateTransactionStatus');
+Route::get('/transaction/{id}/tracking', 'API\TransactionController@getTrackingStatus');
+Route::get('/product/search', 'API\ProductController@searchProduct');
